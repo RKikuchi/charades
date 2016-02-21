49bea34ed4de8e2ac8968e2119a4a449baaa1a41
Rafael Kikuchi <rkikuchi@inoa.com.br>
2016-02-21 20:48:31 -0300

    Settings screen improvements (action bar/icon/values)

--- app/src/main/java/com/pongo/charades/activities/BaseActivity.java
+++ app/src/main/java/com/pongo/charades/activities/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.pongo.charades.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.pongo.charades.CharadesApplication;
